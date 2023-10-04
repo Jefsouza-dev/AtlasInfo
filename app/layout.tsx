0d620c78b46b5e19d7a04c21f36934a381550a54
Jeferson Gonçalves <jefsouza.dev@gmail.com>
2023-10-04 17:08:54 -0300

    Implementation of the home page completed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
 
 const nunitoSans = Nunito_Sans({ subsets: ["latin"] });
 
@@ -16,7 +18,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunitoSans.className}>{children}</body>
+      <body className={nunitoSans.className}>
+        <main className="bg-gray-100 min-h-screen flex flex-col items-center">
+          <nav className="w-full bg-white h-16 flex items-center justify-center">
+            <section className="container flex items-center gap-3">
+              <Link href="/">
+                <Image src="/Logo.svg" alt="logo" width={48} height={48} />
+              </Link>
+              <h1 className="font-bold text-2xl">Atlas Info</h1>
+            </section>
+          </nav>
+          {children}
+        </main>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,49 @@
-export default function Home() {
-  return <h1>HOME</h1>;
+import Image from "next/image";
+
+interface ICountryProps {
+  name: {
+    common: string;
+  };
+  translations: {
+    por: {
+      common: string;
+    };
+  };
+  flags: {
+    svg: string;
+    alt: string;
+  };
+}
+
+async function getCountries(): Promise<ICountryProps[]> {
+  const response = await fetch("https://restcountries.com/v3.1/all");
+  return response.json();
+}
+
+export default async function Home() {
+  const countries = await getCountries();
+
+  return (
+    <section className="container w-full grid grid-cols-5 gap-2 mt-16">
+      {countries.map((country) => (
+        <article
+          key={country.name.common}
+          className="h-64 min-w-full p-2 bg-white border-2 rounded-xl hover:border-indigo-200 transition-all hover:shadow-xl"
+        >
+          <div className="relative w-full h-40 p-2 overflow-hidden  rounded-xl">
+            <Image
+              src={country.flags.svg}
+              alt={country.flags.alt}
+              fill
+              className="object-cover"
+            />
+          </div>
+
+          <h1 className="font-bold text-xl text-center mt-2 ">
+            {country.translations.por.common}
+          </h1>
+        </article>
+      ))}
+    </section>
+  );
 }
